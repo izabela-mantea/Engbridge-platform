6073a5eda528a12e04d64656dffbe647e984233b
elena-izabela.mantea <elena-izabela.mantea@student.tuiasi.ro>
2025-11-11 21:14:11 +0200

    Level_Courses_started

---
+++ engbridge/src/main/java/com/engbridge/controllers/CourseController.java
@@ -0,0 +1,4 @@
+package com.engbridge.controllers;
+
+public class CourseController {
+}
--- engbridge/src/main/java/com/engbridge/model/Level.java
+++ engbridge/src/main/java/com/engbridge/model/Level.java
@@ -1,4 +1,21 @@
 package com.engbridge.model;
+import jakarta.persistence.*;
+import lombok.Data;
 
+import java.util.List;
+
+@Entity
+@Data
+@Table(name = "levels")
 public class Level {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @OneToMany(mappedBy = "levels", fetch = FetchType.LAZY)
+    private List<Course> courses;
+
 }
