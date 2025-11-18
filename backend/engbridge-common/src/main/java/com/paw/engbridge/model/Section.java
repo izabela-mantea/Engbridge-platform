679d3ca65fb69d9482c4c11057cf07a3b9a74f9a
lauraDascalu <dascalulaura27@gmail.com>
2025-11-18 20:49:01 +0200

    added Exercise and Section models

--- backend/engbridge-common/src/main/java/com/paw/engbridge/model/Section.java
+++ backend/engbridge-common/src/main/java/com/paw/engbridge/model/Section.java
@@ -1,4 +1,28 @@
 package com.paw.engbridge.model;
+import jakarta.persistence.*;
+import lombok.Data;
+import java.util.List;
 
+@Entity
+@Data
+@Table(name = "sections")
 public class Section {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column()
+    private Integer orderNum;
+
+    @Column(length = 15)
+    private String type;
+
+    @ManyToOne
+    @JoinColumn(name="sections_course_fk")
+    private Course course;
+
+    @OneToMany(mappedBy = "exercises", fetch = FetchType.LAZY)
+    private List<Exercise> exercises;
+
+
 }
