6ca74ef8369f2f115ba8a3ad906383f40fbf6e8b
elena-izabela.mantea <elena-izabela.mantea@student.tuiasi.ro>
2025-12-06 21:02:21 +0200

    avem footer ?

--- frontend/engbridge-frontend/src/app/app.routes.ts
+++ frontend/engbridge-frontend/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { PlacementTest } from './components/placement-test/placement-test';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/placement-test', pathMatch: 'full' },
+  { path: 'placement-test', component: PlacementTest }
+];
