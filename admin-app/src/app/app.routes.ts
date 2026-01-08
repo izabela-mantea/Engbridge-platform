07935f3b8b5170a88cac3585e9a8894ea79a1d85
elena-izabela.mantea <elena-izabela.mantea@student.tuiasi.ro>
2026-01-08 19:27:28 +0200

    changes on ui

--- admin-app/src/app/app.routes.ts
+++ admin-app/src/app/app.routes.ts
@@ -5,6 +5,7 @@ import { authGuard } from './guards/auth.guard';
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
-  { path: 'admin', component: AdminDashboardComponent, canActivate: [authGuard] },
+  { path: 'admin', component: AdminDashboardComponent,
+     canActivate: [authGuard] },
   { path: '', redirectTo: '/login', pathMatch: 'full' }
 ];
